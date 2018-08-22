ed9f6b86292efd95666d69c53e255163182f236b
x <y@here.com>
2018-08-21 21:39:50 -0600

    Refactored git and vim configuration

--- provision/00-base.sh
+++ provision/00-base.sh
@@ -1,20 +1,53 @@
 
-# Link configs into CONFIG_DIR
-for F in "$CWD"/config/{vim,}; do
-  ln -sfv "$CWD/config/$F" "$CONFIG_DIR"
-done
+function tildePath {
+  echo $(echo $1 | sed "s|^$HOME|~|")
+}
 
-# Create VIM cache dirs because it can't on its own
-mkdir -p "$XDG_CACHE_HOME"/vim/{swap,backup,undo}
+# Create VIM dirs
+mkdir -p "$CONFIG_DIR"/vim
+mkdir -p "$CACHE_DIR"/vim/{swap,backup,undo}
+mkdir -p "$RUNTIME_DIR"/vim
 
-# Copy data directories
+# VIM configuration to support XDG DIRS
+if [ ! -f "$CONFIG_DIR"/vim/config ]; then
+cat << EOF > "$CONFIG_DIR"/vim/config
+set directory=$(tildePath $CACHE_DIR/vim/swap)
+set backupdir=$(tildePath $CACHE_DIR/vim/backup)
+set undodir=$(tildePath $CACHE_DIR/vim/undo)
+set viminfo+=n$(tildePath $CACHE_DIR/vim/viminfo)
+set runtimepath=$RUNTIME_DIR/vim
+EOF
+fi
+
+echo "Copying data directories"
 for F in "$CWD"/{data,data-"$OS"}; do
   [ -d "$F" ] && cp -nvr "$F"/* "$DATA_DIR"/
 done
 
-# Copy configs which must be manually changed
-for F in "$CWD"/config/{git,}; do
-  cp -nvr "$CWD/config/$F" "$CONFIG_DIR"/
-done
+# Copy config directories which must differ from repo version
+echo "Copying config directories"
+cp -nvr "$CWD"/config/git "$CONFIG_DIR"/
 
-# TODO: Prompt for git config values
+# Back up and relocate replaced configs
+echo "Backing up configuration files"
+[ -f ~/.gitconfig ] && mv ~/.gitconfig ~/.gitconfig.bak
+
+# Finish Git configuration
+echo "Finishing Git configuration"
+git config -f "$CONFIG_DIR"/git/config core.excludesfile $(tildePath "$CONFIG_DIR"/git/excludes)
+[ -n "$FULL_NAME" ] && git config -f "$CONFIG_DIR"/git/config user.name "$FULL_NAME"
+[ -n "$EMAIL_ADDRESS" ] && git config -f "$CONFIG_DIR"/git/config user.email "$EMAIL_ADDRESS"
+if [ -n $(which meld) ]; then
+  git config -f "$CONFIG_DIR"/git/config diff.tool meld
+  git config -f "$CONFIG_DIR"/git/config merge.tool meld
+elif [ -n $(which diffmerge) ]; then
+  git config -f "$CONFIG_DIR"/git/config diff.tool diffmerge
+  git config -f "$CONFIG_DIR"/git/config merge.tool diffmerge
+  git config -f "$CONFIG_DIR"/git/config difftool.diffmerge.cmd "diffmerge \$LOCAL \$REMOTE"
+  git config -f "$CONFIG_DIR"/git/config merge.diffmerge.cmd "diffmerge \$LOCAL \$MERGED \$REMOTE --output \$MERGED"
+elif [ -x /Applications/DiffMerge.appContents/MacOS/diffmerge ]; then
+  git config -f "$CONFIG_DIR"/git/config diff.tool diffmerge
+  git config -f "$CONFIG_DIR"/git/config merge.tool diffmerge
+  git config -f "$CONFIG_DIR"/git/config difftool.diffmerge.cmd "/Applications/DiffMerge.appContents/MacOS/diffmerge \$LOCAL \$REMOTE"
+  git config -f "$CONFIG_DIR"/git/config merge.diffmerge.cmd "/Applications/DiffMerge.appContents/diffmerge \$LOCAL \$MERGED \$REMOTE --output \$MERGED"
+fi
