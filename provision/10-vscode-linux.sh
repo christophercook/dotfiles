9076d5d00c227aca7f3d6c1d3b2cefb67975df71
Christopher Cook <beerfan@gmail.com>
2018-08-22 00:27:02 -0600

    Added extension installation for vscode

--- provision/10-vscode-linux.sh
+++ provision/10-vscode-linux.sh
@@ -7,4 +7,27 @@ if [ ! -e /etc/apt/sources.list.d/vscode.list ]; then
 fi
 
 # Install
-[ -z "$(which code)" ] && sudo apt-get install code
+[ -z "$(which code)" ] && sudo apt-get install -y code
+
+# Get installed extensions (lowercase)
+declare -a installed_extensions
+IFS=$'\r\n' GLOBIGNORE='*' command eval 'installed_extensions=($(code --list-extensions | tr [:upper:] [:lower:]))'
+
+# Define extensions to be installed
+declare -a all_extensions
+all_extensions=(
+  mikestead.dotenv
+  dbaeumer.vscode-eslint
+  sidneys1.gitconfig
+  zignd.html-css-class-completion
+  davidanson.vscode-markdownlint
+  2gua.rainbow-brackets
+  editorconfig.editorconfig
+)
+
+# Install extensions that are not already installed
+for E in ${all_extensions[@]}; do
+  if [[ ! " ${installed_extensions[@]} " =~ " ${E} " ]]; then
+    code --install-extension "$E"
+  fi
+done
