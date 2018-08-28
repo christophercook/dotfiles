84e517db9813d3ac02e101a526591d4e9c656ae9
Christopher Cook <beerfan@gmail.com>
2018-08-28 13:21:12 -0600

    Fixed order issue with nodejs package

--- packages/20-nodejs.sh
+++ packages/20-nodejs.sh
@@ -1,3 +1,19 @@
+# Set up author info using Git config
+if [ -z "$(npm config get init-author-name)" ]; then
+  FULL_NAME="$(git config --get user.name)"
+  [ -n "$FULL_NAME" ] && npm config set init-author-name "$FULL_NAME"
+fi
+if [ -z "$(npm config get init-author-email)" ]; then
+  EMAIL_ADDRESS="$(git config --get user.email)"
+  [ -n "$EMAIL_ADDRESS" ] && npm config set init-author-email "$EMAIL_ADDRESS"
+fi
+
+# Other configs?
+npm config set optional false
+#npm config init-module "$CONFIG_DIR"/npm/npm-init.js
+#npm config set init-author-url ""
+#npm config set init-license "ISC"
+
 # Install some global modules
 yarn_modules=(
   eslint
